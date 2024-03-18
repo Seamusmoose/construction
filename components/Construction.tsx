5858afac0c7e9018ed84c843bc51ec2c31ea363f
Seamus OSullivan <info@seamuso.com>
2024-03-18 15:07:53 +0100

    text changes

--- components/Construction.tsx
+++ components/Construction.tsx
@@ -8,7 +8,17 @@ const Construction: React.FC = () => {
         <div className="center">
           <div className={styles.animation}></div>
         </div>
-        <h1 className={styles.title}>Under Construction</h1>
+        <h1 className={styles.title}>Elvira Glück</h1>
+        <h3 className={styles.titleSecondary}>
+          Systemische Einzel - Paar - und Familientherapie
+        </h3>
+        <h3 className={styles.titleSecondary}>Supervision</h3>
+        <h3 className={styles.titleSecondary}>ECP</h3>
+
+        <h3 className={styles.titleSecondary}>+49 30 2101 8633</h3>
+
+        <br />
+        <h3 className={styles.titleSecondary}>Under Construction</h3>
         <p className={styles.description}>
           We are currently working on this page. Please check back later!
         </p>
